c605461c0553f0bc377eaae727b26e05b0d45d14
Schischu <schischu65@gmail.com>
2015-12-08 20:35:17 +0100

    device.mk: Copy mixer_paths.xml

--- device.mk
+++ device.mk
@@ -16,6 +16,9 @@
 
 $(call inherit-product, device/samsung/chagall-klimt-common/device.mk)
 
+PRODUCT_COPY_FILES += \
+    device/samsung/chagallwifi/audio/mixer_paths.xml:system/etc/mixer_paths.xml
+
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml
